b357defb17fe07498b551eed92cb8d5a765b28b5
Dhiraj Kumar <161637836+Dhirajkumarmahto@users.noreply.github.com>
2024-03-06 23:42:41 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+    
+    public void eat(){
+        System.out.print("I AM ETTING ");
+    }
+    public static void main(String[] args){
+        Animal buzo=new Animal();
+        buzo.eat();
+    }
+}
