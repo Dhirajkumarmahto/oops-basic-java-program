6e528fc1321942c7d6376bae1b6a6bf01e772ab4
Dhiraj Kumar <161637836+Dhirajkumarmahto@users.noreply.github.com>
2024-03-11 00:07:36 +0530

    Add files via upload

---
+++ static.java
@@ -0,0 +1,13 @@
+class Test // class name 
+{
+    static int a=10; // static variable
+}
+
+class display  // second class
+{
+    public static void main(String[] args) //main method
+    {
+        System.out.print(Test.a); // display
+        
+    }
+}
